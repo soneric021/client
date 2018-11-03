a1c73bc8a15b845d53f8486e437d228d29deee6a
soneric021 <soneric021@gmail.com>
2018-11-03 01:03:52 -0400

    initial commit frontend

---
+++ src/components/home.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-container>
+        {{datos}}
+    </v-container>
+</template>
+<script>
+import axios from 'axios';
+export default {
+    data: () => ({
+        datos: null
+    }),
+    mounted(){
+        this.mostrarEmpleos();
+    },
+    methods: {
+        mostrarEmpleos(){
+            axios.get('http://localhost/pempleos/empleo/listar')
+            .then(response=> {
+                this.datos = response.data
+                })
+            .catch(err=>this.datos = err);
+        }
+    }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
+import axios from 'axios'
+import VueCookies from 'vue-cookies';
+import VueSession from 'vue-session';
+import VueRouter from 'vue-router';
+
+//import routes from './routes';
 
 Vue.config.productionTip = false
+Vue.use(axios)
+Vue.use(VueCookies)
+Vue.use(VueSession)
+Vue.use(VueRouter)
 
+const routes = [
+  {path: '/', component:App }
+]
+const router = new VueRouter({
+  routes
+})
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
